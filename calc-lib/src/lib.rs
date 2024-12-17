a09ca8caec66d6d12cacdf6d3cad9fea0be16b5b
Ojas Kavathe <ojaskavathe@gmail.com>
2024-12-17 15:42:03 +0530

    wasm: truncate results

--- calc-lib/src/lib.rs
+++ calc-lib/src/lib.rs
@@ -5,7 +5,11 @@ use meval::*;
 pub fn calculate(expr: &str) -> String {
     match eval_str(expr) {
         Ok(result) => {
-            result.to_string()
+            if result.abs() >= 1e10 || result.abs() <= 1e-10 && result != 0.0 {
+                format!("{:.10e}", result) // if numbers are too long
+            } else {
+                result.to_string()
+            }
         }
         Err(_) => String::from("Error: Could not parse!")
     }
