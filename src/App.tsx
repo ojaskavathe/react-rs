94428b41a928f45e9dfea6047bd96fbdbfb42dba
Ojas Kavathe <ojaskavathe@gmail.com>
2024-12-17 15:37:43 +0530

    react: shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,54 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 
 import init, { calculate } from "calc-lib";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./App.css"
+
+export default function App() {
+  const [ans, setAns] = useState("");
+  const [expr, setExpr] = useState("");
 
   useEffect(() => {
     init().then(() => {
-      console.log(calculate("10/2"))
+      console.log("wasm-loaded");
     });
   }, []);
 
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Card>
+      <CardHeader>
+        <CardTitle>Calculator</CardTitle>
+        <CardDescription>Evaluate Expressions</CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <div className="space-y-2 flex flex-col items-center sm:space-x-2 sm:space-y-0 sm:flex-row">
+          <Input
+            id="name"
+            placeholder="Expression (eg. 2+5)"
+            value={expr}
+            onChange={(e) => setExpr(e.target.value)}
+          />
+          <Button onClick={() => setAns(calculate(expr))}>Calculate</Button>
+        </div>
+        {ans != "" && (
+          <div className="space-y-1 flex items-center space-x-2">
+            <Label>Answer:</Label>
+            <Input value={ans} readOnly />
+          </div>
+        )}
+      </CardContent>
+    </Card>
+  );
 }
 
-export default App
