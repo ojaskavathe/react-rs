899f8601ae261d1e12d6550a2e3f4b1a4b359378
Ojas Kavathe <ojaskavathe@gmail.com>
2024-12-17 14:56:38 +0530

    init: empty vite project

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "wasm-react";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              nodejs
+
+              nodePackages.typescript-language-server
+              nodePackages.prettier
+              vscode-langservers-extracted
+            ];
+          };
+      }
+    );
+}
