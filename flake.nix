5a9d0a7354e4a12e4f6d726afafb10c373ac093d
Ojas Kavathe <ojaskavathe@gmail.com>
2024-12-17 15:14:55 +0530

    wasm: rust library + wasm export

--- flake.nix
+++ flake.nix
@@ -3,20 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       nixpkgs,
+      rust-overlay,
       flake-utils,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
+        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system;
+          inherit system overlays;
         };
       in
       {
@@ -24,7 +27,13 @@
           with pkgs;
           mkShell {
             buildInputs = [
+              (rust-bin.stable.latest.default.override {
+                targets = ["wasm32-unknown-unknown"];
+              })
+              rust-analyzer
+
               nodejs
+              wasm-pack
 
               nodePackages.typescript-language-server
               nodePackages.prettier
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import init, { add } from "calc-lib";
+
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    init().then(() => {
+      console.log(add(1, 3))
+    });
+  }, []);
+
 
   return (
     <>
