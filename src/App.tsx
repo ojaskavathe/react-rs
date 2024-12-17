353cbeaf56fd873500b2319910bee8296be41cee
Ojas Kavathe <ojaskavathe@gmail.com>
2024-12-17 15:49:40 +0530

    react: tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -10,10 +12,9 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-
 import init, { calculate } from "calc-lib";
 
-import "./App.css"
+import "./App.css";
 
 export default function App() {
   const [ans, setAns] = useState("");
@@ -26,29 +27,39 @@ export default function App() {
   }, []);
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Calculator</CardTitle>
-        <CardDescription>Evaluate Expressions</CardDescription>
-      </CardHeader>
-      <CardContent className="space-y-2">
-        <div className="space-y-2 flex flex-col items-center sm:space-x-2 sm:space-y-0 sm:flex-row">
-          <Input
-            id="name"
-            placeholder="Expression (eg. 2+5)"
-            value={expr}
-            onChange={(e) => setExpr(e.target.value)}
-          />
-          <Button onClick={() => setAns(calculate(expr))}>Calculate</Button>
-        </div>
-        {ans != "" && (
-          <div className="space-y-1 flex items-center space-x-2">
-            <Label>Answer:</Label>
-            <Input value={ans} readOnly />
-          </div>
-        )}
-      </CardContent>
-    </Card>
+    <Tabs defaultValue="shadergen" className="w-auto">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="calculator">Calculator</TabsTrigger>
+        <TabsTrigger value="shadergen">ShaderGen</TabsTrigger>
+      </TabsList>
+      <TabsContent value="calculator">
+        <Card>
+          <CardHeader>
+            <CardTitle>Calculator</CardTitle>
+            <CardDescription>Evaluate Expressions</CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-2 flex flex-col items-center sm:space-x-2 sm:space-y-0 sm:flex-row">
+              <Input
+                id="name"
+                placeholder="Expression (eg. 2+5)"
+                value={expr}
+                onChange={(e) => setExpr(e.target.value)}
+              />
+              <Button onClick={() => setAns(calculate(expr))}>Calculate</Button>
+            </div>
+            {ans != "" && (
+              <div className="space-y-1 flex items-center space-x-2">
+                <Label>Answer:</Label>
+                <Input value={ans} readOnly />
+              </div>
+            )}
+          </CardContent>
+        </Card>
+      </TabsContent>
+      <TabsContent value="shadergen">
+        <div>placeholder</div>
+      </TabsContent>
+    </Tabs>
   );
 }
-
